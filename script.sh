f924f55a3bc19c3f164034d85e497a4ce85c1623
sacha.perdrizat <sacha.perdrizat@gmail.com>
2020-10-09 22:45:23 +0200

    update script

--- script.sh
+++ script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+containerName='sti_project'
+
+if [ -n "$(docker ps -aq --filter "name=${containerName}")" ]
+then
+  echo "suppression du container existant"
+  docker rm $(docker stop $(docker ps -aq --filter "name=${containerName}"))
+fi
+
+
 docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
 
 docker exec sti_project service nginx start
