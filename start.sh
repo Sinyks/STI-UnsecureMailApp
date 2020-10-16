78b1d0897178806e64bd79c486ed33415f68c1f8
sacha.perdrizat <sacha.perdrizat@gmail.com>
2020-10-16 22:42:24 +0200

    rename script

---
+++ start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+containerName='sti_project'
+
+if [ -n "$(docker ps -aq --filter "name=${containerName}")" ]
+then
+  echo "suppression du container existant"
+  docker rm $(docker stop $(docker ps -aq --filter "name=${containerName}"))
+fi
+
+chmod -R a+wX site/databases/
+
+docker run -ti -v "$PWD/site":/usr/share/nginx/ -d -p 8080:80 --name sti_project --hostname sti arubinst/sti:project2018
+
+docker exec sti_project service nginx start
+docker exec sti_project service php5-fpm start
